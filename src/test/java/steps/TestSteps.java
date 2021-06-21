30c768b81f35a15282cbb68db225214513443eb8
Xannara <roksanasky@gmail.com>
2021-06-22 01:46:50 +0300

    Добавлены первые две заявки

--- src/test/java/steps/TestSteps.java
+++ src/test/java/steps/TestSteps.java
@@ -5,8 +5,7 @@ import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestSteps {
@@ -337,5 +336,74 @@ public class TestSteps {
 
         $(".header-button-bar__menu .icon_name_arrow-down").click();
         $(".header-popup-menu__item-button .icon_name_logout").click();
+    }
+
+    @Step("Заявка на реструктуризацию")
+    public void request1() {
+        $(".button-send-request").click();
+        Selenide.switchTo().window("Запросы | Альфа-лизинг");
+       // $x("//*[contains(text(), 'Создать новый')]").click();
+        $(byText("Выберите категорию запроса. Заполните дополнительные поля")).shouldBe(visible);
+        $(".select-button__content").click();
+        $(".menu-item__control:nth-child(1)").click();
+        $(".select-contracts__field").click();
+        $(".item:nth-child(3)").click();
+        $(byText("Выберите категорию запроса. Заполните дополнительные поля")).click();
+        $x("//*[contains(text(), 'Продолжить')]").click();
+        $(byAttribute("value","Дриневская  Юлия Юрьевна")).shouldBe(visible);
+        $(byAttribute("value","Директор")).shouldBe(visible);
+        $(byAttribute("value","vaguz@alfaleasing.ru")).shouldBe(visible);
+        $(byAttribute("value","+7 (960) 990-15-25")).shouldBe(visible);
+        $x("//*[contains(text(), 'Продолжить')]").click();
+
+        $(byText("Для быстрого рассмотрения, пожалуйста, загрузите следующие документы")).shouldBe(visible);
+        $("#certificateBank").uploadFromClasspath("readme.txt");
+
+        $(".textarea__inner .textarea__control").click();
+        $(".textarea__inner .textarea__control").setValue("Заявка на реструктуризацию создана автотестом");
+        $(byText("Падение курса рубля")).click();
+        $(byText("Не наблюдается, снижение выручки не ожидается")).click();
+        $(byText("Выручки хватает для исполнения обязательств")).click();
+        $(byText("По договорам / поставкам / услугам 2019 года или ранее")).click();
+        $(byText("Задержки оплаты от контрагентов")).click();
+        $(".textarea__inner .textarea__control", 1).click();
+        $(".textarea__inner .textarea__control", 1).setValue("Заявка на реструктуризацию создана автотестом");
+        $x("//*[contains(text(), 'Продолжить')]").click();
+
+        $(byText("Каникулы по уплате основного долга на 3 месяца. В течение трех месяцев вы будете уплачивать только проценты по договору лизинга")).click();
+        $(byText("Часть платежей, которая не была уплачена в течение каникул, будет равномерно распределена в течение оставшегося срока лизинга и добавлена к лизинговым платежам")).click();
+        $(".textarea__inner .textarea__control").click();
+        $(".textarea__inner .textarea__control").setValue("Заявка на реструктуризацию создана автотестом");
+        $x("//*[contains(text(), 'Отправить запрос')]").click();
+
+        $x("//*[contains(text(), 'Вернуться в личный кабинет')]").click();
+    }
+
+    @Step("Справка")
+    public void request2() {
+        $(".button-send-request").click();
+        Selenide.switchTo().window("Запросы | Альфа-лизинг");
+        //$x("//*[contains(text(), 'Создать новый')]").click();
+        $(byText("Выберите категорию запроса. Заполните дополнительные поля")).shouldBe(visible);
+        $(".select-button__content").click();
+        $(".menu-item:nth-child(2)").click();
+        $(".select-contracts__field").click();
+        $(".item:nth-child(3)").click();
+        $(byText("Выберите категорию запроса. Заполните дополнительные поля")).click();
+        $x("//*[contains(text(), 'Продолжить')]").click();
+        $(byAttribute("value","Дриневская  Юлия Юрьевна")).shouldBe(visible);
+        $(byAttribute("value","Директор")).shouldBe(visible);
+        $(byAttribute("value","vaguz@alfaleasing.ru")).shouldBe(visible);
+        $(byAttribute("value","+7 (960) 990-15-25")).shouldBe(visible);
+        $x("//*[contains(text(), 'Продолжить')]").click();
+
+        $x("//*[contains(text(), 'ООО «Альфамобиль»')]").click();
+        $(byText("Справка об отсутствии задолженности (кредитной дисциплине / остатке лизинговых платежей)")).click();
+        $(".input__inner .input__control").click();
+        $(".input__inner .input__control").setValue("22.06.2021");
+        $(".radio_size_m", 7).click();
+        $(".input__inner .input__control", 1).click();
+        $(".input__inner .input__control", 1).setValue("Moscow");
+        $x("//*[contains(text(), 'Отправить запрос')]").click();
     }
 }
